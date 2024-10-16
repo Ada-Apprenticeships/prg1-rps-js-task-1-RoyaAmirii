3d6c1044484f06b08b161a35ee0994b3b587d374
RoyaAmirii <roya.amiri@ada.ac.uk>
2024-10-16 14:37:16 +0000

    draw7

--- rps.js
+++ rps.js
@@ -1,21 +1,27 @@
 function rockPaperScissors(player1, player2) {
+  
   if (player1 === player2) {
     return "draw" ;   
+  
   } else if ((player1 === "paper" && player2 === "rock") || // - '||' is a js operator
             (player1 === "paper" && player2 === "spock") ||
-           (player1 ==="scissors" && player2 === "paper") || // - '===' is used when 2 objects are being compared
+           
+            (player1 ==="scissors" && player2 === "paper") || // - '===' is used when 2 objects are being compared
            (player1 ==="scissors" && player2 === "lizard") ||
+           
            (player1 ==="rock" && player2 === "scissors") ||
            (player1 === "rock" && player2 === "lizard") ||
+           
            (player1 ==="lizard" && player2 === "paper") ||
            (player1 === "lizard" && player2 === "spock") ||
+           
            (player1 ==="spock" && player2 === "rock") ||
            (player1 === "spock" && player2 === "scissors")
-           ){
-            return "player1"
-          } else {
-            return "player2"
-          }
+           )
+           { return "player1"}
+           
+           else {return "player2"}
+          
 }
 console.log(rockPaperScissors());
 
